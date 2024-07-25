5779bcdb29f5cef5d722630761c1bc9a8df109d7
unozer <40232537+unozer@users.noreply.github.com>
2024-07-25 13:49:42 +0200

    First commit

---
+++ src/main/java/it/gp/db_relations/DbRelationsApplication.java
@@ -0,0 +1,13 @@
+package it.gp.db_relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRelationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gp/db_relations/DbRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gp.db_relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

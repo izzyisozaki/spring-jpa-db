6df47509b916794cd4f82cec5100cc89d730dfae
unozer <40232537+unozer@users.noreply.github.com>
2024-07-25 14:11:21 +0200

    generate classi e tabelle User e UserDetails

---
+++ src/main/java/it/gp/db_relations/model/User.java
@@ -0,0 +1,24 @@
+package it.gp.db_relations.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToOne(mappedBy = "user")
+    private UserDetails userDetails;
+}
---
+++ src/main/java/it/gp/db_relations/model/UserDetails.java
@@ -0,0 +1,24 @@
+package it.gp.db_relations.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private String phoneNumber;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}

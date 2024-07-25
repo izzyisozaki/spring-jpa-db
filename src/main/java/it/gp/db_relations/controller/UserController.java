1aae80a0b52a5e7dd1d13a8880abda94acc1ba65
unozer <40232537+unozer@users.noreply.github.com>
2024-07-25 17:08:12 +0200

    aggiunto supporto documentazione OpenApi

--- src/main/java/it/gp/db_relations/controller/UserController.java
+++ src/main/java/it/gp/db_relations/controller/UserController.java
@@ -1,5 +1,10 @@
 package it.gp.db_relations.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gp.db_relations.model.User;
 import it.gp.db_relations.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +15,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Gestione Utenti", description = "Api di gestione utenti")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
+    @Operation(summary = "Recupera utenti", description = "Torna la lista di tutti gli utenti sul database")
+    @ApiResponse(responseCode = "200", description = "Utenti recuperati con successo")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    @Operation(summary = "Recupera utente per id", description = "Questo endpoint torna l'utente che ha id passato in ingresso, se presente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente trovato"),
+            @ApiResponse(responseCode = "404", description = "Utente non trovato con l'id specificato")
+    })
+    public ResponseEntity<User> getUserById(
+            @Parameter(name = "id", required = true, description = "Identificativo dell'utente, corrisponde alla primary key del database")
+            @PathVariable Long id
+    ) {
         User user = userRepository.findById(id).orElse(null);
         if (user == null)
             return ResponseEntity.notFound().build();
